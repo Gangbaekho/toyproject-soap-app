c8c1ee1ede3d82aa421cd43ad888a37e89f84755
kimjinsoo <roorooro9933@gmail.com>
2021-01-20 10:50:23 +0900

    styling the product items

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
 
   return (
     <FlatList
+      // style={styles.screen}
       keyExtractor={(item) => item.id}
       data={products}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
     />
   );
 };
@@ -18,6 +28,10 @@ ProductsOverviewScreen.navigationOptions = {
   headerTitle: "All products",
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  // screen: {
+  //   flex: 1,
+  // },
+});
 
 export default ProductsOverviewScreen;
