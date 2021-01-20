1297e29b6ea7c7c306ae4223e4270c11f98269ca
kimjinsoo <roorooro9933@gmail.com>
2021-01-20 14:23:28 +0900

    adding logic to delete items

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { View, Text, FlatList, Button, StyleSheet } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
+import * as cartActions from "../../store/actions/cart";
 
 const CartScreen = (props) => {
   const cartTotalAmount = useSelector((state) => state.carts.totalAmount);
@@ -18,8 +19,12 @@ const CartScreen = (props) => {
         sum: state.carts.items[key].sum,
       });
     }
-    return transformedCartItems;
+    return transformedCartItems.sort((a, b) =>
+      a.productId > b.productId ? 1 : -1
+    );
   });
+
+  const dispatch = useDispatch();
 
   return (
     <View style={styles.screen}>
@@ -42,7 +47,9 @@ const CartScreen = (props) => {
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
             amount={itemData.item.sum}
-            onRemove={() => {}}
+            onRemove={() => {
+              dispatch(cartActions.removeFromCart(itemData.item.productId));
+            }}
           />
         )}
       />
--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../actions/cart";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from "../../models/cart-item";
 
 const initialState = {
@@ -36,12 +36,36 @@ export default (state = initialState, action) => {
         );
         return {
           ...state,
-          //  [] 를 이용해서 key를 만들 수 있다 정도?
-          //   이것만 새롭게 알면 될 듯.
           items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
           totalAmount: state.totalAmount + productPrice,
         };
       }
+    case REMOVE_FROM_CART:
+      const selectedCartItem = state.items[action.pid];
+      const currentQty = state.items[action.pid].quantity;
+      let updatedCartItems;
+      if (currentQty > 1) {
+        const updatedCartItem = new CartItem(
+          selectedCartItem.quantity - 1,
+          selectedCartItem.productPrice,
+          selectedCartItem.productTitle,
+          selectedCartItem.sum - selectedCartItem.productPrice
+        );
+        // 동적으로 key값과 value  값을 추가하는 방법에 대해서도
+        // 알아두면 좋다. 아니 알아야 한다.
+        updatedCartItems = { ...state.items, [action.pid]: updatedCartItem };
+      } else {
+        updatedCartItems = { ...state.items };
+        //   이거 처음보는거니까 알아두자.
+        // Object의 key를 기준으로 날리고 싶으면 delete라는
+        // keyword를 써주면 된다는 것 정도 알고 넘어가면 될 듯.
+        delete updatedCartItems[action.pid];
+      }
+      return {
+        ...state,
+        items: updatedCartItems,
+        totalAmount: state.totalAmount - selectedCartItem.productPrice,
+      };
   }
   return state;
 };
