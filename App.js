0db5b2eff6c5f64fae120826deb63e574adefd46
kimjinsoo <roorooro9933@gmail.com>
2021-01-20 10:13:39 +0900

    setting up a navigator

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import ShopNavigator from "./navigation/ShopNaviagator";
 
 import productsReducer from "./store/reducers/products";
 
@@ -15,18 +14,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
-        <Text>This is root screen</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import { useSelector } from "react-redux";
 
 const ProductsOverviewScreen = (props) => {
+  const products = useSelector((state) => state.products.availableProducts);
+
   return (
-    <View>
-      <Text>This is products overview screen.</Text>
-    </View>
+    <FlatList
+      keyExtractor={(item) => item.id}
+      data={products}
+      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+    />
   );
 };
 
