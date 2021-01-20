0fe7a64a284204fa6286b290d033d84357d540e0
kimjinsoo <roorooro9933@gmail.com>
2021-01-20 15:07:25 +0900

    side drawer and orders screen

--- navigation/ShopNaviagator.js
+++ navigation/ShopNaviagator.js
@@ -1,10 +1,27 @@
+import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { Platform } from "react-native";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import CartScreen from "../screens/shop/CartScreen";
+import OrdersScreen from "../screens/shop/OrdersScreen";
 import Colors from "../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+  headerTitleStyle: {
+    fontFamily: "open-sans-bold",
+  },
+  headerBackTitle: {
+    fontFamily: "open-sans",
+  },
+};
 
 const ProductsNavigator = createStackNavigator(
   {
@@ -13,19 +30,49 @@ const ProductsNavigator = createStackNavigator(
     Cart: CartScreen,
   },
   {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
-      },
-      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
-      headerTitleStyle: {
-        fontFamily: "open-sans-bold",
-      },
-      headerBackTitle: {
-        fontFamily: "open-sans",
-      },
+    navigationOptions: {
+      drawerIcon: (drawerConfig) => (
+        <Ionicons
+          name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
+    },
+    defaultNavigationOptions: defaultNavOptions,
+  }
+);
+
+const OrdersNavigator = createStackNavigator(
+  {
+    Orders: OrdersScreen,
+  },
+  {
+    // Drawer Navigator에다가 각각의 Stack Navigator의
+    // 아이콘을 설정해주기 위해서는 여기다가 아래와 같은 설정을 하면 된다는 것이다.
+    navigationOptions: {
+      drawerIcon: (drawerConfig) => (
+        <Ionicons
+          name={Platform.OS === "android" ? "md-list" : "ios-list"}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
+    },
+    defaultNavigationOptions: defaultNavOptions,
+  }
+);
+
+const ShopNavigator = createDrawerNavigator(
+  {
+    Products: ProductsNavigator,
+    Orders: OrdersNavigator,
+  },
+  {
+    contentOptions: {
+      activeTintColor: Colors.primary,
     },
   }
 );
 
-export default createAppContainer(ProductsNavigator);
+export default createAppContainer(ShopNavigator);
